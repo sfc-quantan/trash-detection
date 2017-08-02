6fa6a5b6a335d6cb803d11fc900308c50b4d3d08
Kazuhiro Mikami <quantan@net79-dhcp245.sfc.keio.ac.jp>
2017-08-02 13:50:00 +0900

    PEP8とFlake8に基づいて修正

--- create_testdata.py
+++ create_testdata.py
@@ -3,38 +3,33 @@ import numpy as np
 import json
 import matplotlib.pyplot as Plt
 
-k=1367
-width=480
-height=640
-gominum=7
-xywhc=5
+k = 1367
+width = 480
+height = 640
+gominum = 7
+xywhc = 5
 
 
-y=np.zeros((k,gominum,xywhc))
-x=np.arange(k*width*height*3).reshape(k,width,height,3)
+y = np.zeros((k, gominum, xywhc))
+x = np.arange(k*width*height*3).reshape(k, width, height, 3)
 
-for i in range(1,k+1):
-    img=Plt.imread("./1/"+str(i)+".jpg")    
-    x[i-1]=img
+for i in range(1, k+1):
+    img = Plt.imread("./1/"+str(i)+".jpg")
+    x[i-1] = img
 
-for i in range(1,k+1):
-    f=open("./1/annotations/"+str(i)+".json","r")
-    json_dict=json.load(f)
-    tmp = len(json_dict["objects"])    
+for i in range(1, k+1):
+    f = open("./1/annotations/"+str(i)+".json", "r")
+    json_dict = json.load(f)
+    tmp = len(json_dict["objects"])
     for j in range(tmp):
-        y[i-1][j]=json_dict["objects"][j]["x_y_w_h"]+[1]
+        y[i-1][j] = json_dict["objects"][j]["x_y_w_h"]+[1]
 
-z=np.zeros(k)
-for i in range(1,k+1):   
-    f=open("./1/annotations/"+str(i)+".json","r")
-    json_dict=json.load(f)
-    z[i-1]= int(len(json_dict["objects"]))
+z = np.zeros(k)
+for i in range(1, k+1):
+    f = open("./1/annotations/"+str(i)+".json", "r")
+    json_dict = json.load(f)
+    z[i-1] = int(len(json_dict["objects"]))
 
 np.save('x.npy', x)
 np.save('y.npy', y)
-np.save('z.npy',z)
-
-
-
-
-
+np.save('z.npy', z)
